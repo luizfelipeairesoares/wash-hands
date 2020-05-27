7a2dd32c5edeb0455fe1b2a824a4f1aa017ad079
luizfelipeairesoares <lfelipeas@gmail.com>
2020-05-26 22:07:58 -0300

    :art: Adds timed progress, animates transitions and show hands icon

--- washands/Views/CircularProgressView.swift
+++ washands/Views/CircularProgressView.swift
@@ -9,8 +9,14 @@
 import UIKit
 
 class CircularProgressView: UIView {
+    
+    // MARK: - Private properties
 
+    private let color: UIColor!
     private let shapeLayer = CAShapeLayer()
+    private let trackLayer = CAShapeLayer()
+    
+    private var timer: Timer?
     
     private let label: UILabel = {
         let label = UILabel()
@@ -20,10 +26,19 @@ class CircularProgressView: UIView {
         return label
     }()
     
+    private let imageView: UIImageView = {
+        let imgView = UIImageView()
+        imgView.image = UIImage(named: "icon_hands")
+        imgView.contentMode = .scaleAspectFit
+        imgView.alpha = 0.0
+        return imgView
+    }()
+    
     // MARK: - Init
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
+    init(with color: UIColor) {
+        self.color = color
+        super.init(frame: .zero)
     }
     
     required init?(coder: NSCoder) {
@@ -34,22 +49,44 @@ class CircularProgressView: UIView {
         super.draw(rect)
         addCircularShapeLayer()
         addLabel()
+        addImageView()
     }
     
     // MARK: - Public Functions
     
-    public func updateLabel(_ text: String) {
-        label.text = text
-    }
-    
-    public func startAnimating() {
-        let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
-        basicAnimation.toValue = 1
-        basicAnimation.duration = 2
-        basicAnimation.fillMode = .forwards
-        basicAnimation.isRemovedOnCompletion = false
-        
-        shapeLayer.add(basicAnimation, forKey: "basic")
+    public func startAnimating(duration: TimeInterval) {
+        if timer == nil {
+            shapeLayer.strokeColor = color.cgColor
+            
+            let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+            basicAnimation.toValue = 1
+            basicAnimation.duration = duration
+            basicAnimation.fillMode = .forwards
+            basicAnimation.isRemovedOnCompletion = true
+            
+            var elapsedTime = duration
+            label.text = "\(Int(elapsedTime))"
+            timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { [weak self] (tmr) in
+                DispatchQueue.main.async { [weak self] in
+                    if elapsedTime > 1 {
+                        elapsedTime -= 1
+                        self?.label.text = "\(Int(elapsedTime))"
+                    } else {
+                        tmr.invalidate()
+                        self?.label.text = "Start"
+                        self?.timer = nil
+                        self?.showTimerEndAnimation()
+                    }
+                }
+            })
+            
+            UIView.animate(withDuration: 0.5, delay: 0.0, options: .curveLinear, animations: {
+                self.label.frame.origin.y += 40.0
+                self.imageView.alpha = 1.0
+            }) { (completed) in
+                self.shapeLayer.add(basicAnimation, forKey: "forward")
+            }
+        }
     }
     
     // MARK: - Private functions
@@ -62,15 +99,15 @@ class CircularProgressView: UIView {
                                         clockwise: true)
         circularPath.apply(CGAffineTransform(translationX: bounds.width / 2, y: bounds.height / 2))
         
-        let trackLayer = CAShapeLayer()
         trackLayer.frame = frame
         trackLayer.bounds = bounds
         trackLayer.position = CGPoint(x: layer.bounds.midX, y: layer.bounds.midY)
         trackLayer.path = circularPath.cgPath
-        trackLayer.strokeColor = UIColor.red.withAlphaComponent(0.25).cgColor
+        trackLayer.strokeEnd = 1
+        trackLayer.strokeColor = color.withAlphaComponent(0.25).cgColor
         trackLayer.lineWidth = 10
-        trackLayer.fillColor = UIColor.clear.cgColor
         trackLayer.lineCap = .round
+        trackLayer.fillColor = UIColor.clear.cgColor
         
         layer.addSublayer(trackLayer)
         
@@ -79,7 +116,7 @@ class CircularProgressView: UIView {
         shapeLayer.position = CGPoint(x: layer.bounds.midX, y: layer.bounds.midY)
         shapeLayer.path = circularPath.cgPath
         shapeLayer.strokeEnd = 0
-        shapeLayer.strokeColor = UIColor.red.cgColor
+        shapeLayer.strokeColor = color.cgColor
         shapeLayer.lineWidth = 10
         shapeLayer.lineCap = .round
         shapeLayer.fillColor = UIColor.clear.cgColor
@@ -91,6 +128,30 @@ class CircularProgressView: UIView {
     private func addLabel() {
         addSubview(label)
         label.frame = CGRect(x: 0, y: 0, width: bounds.width, height: bounds.height)
+    }
+    
+    private func addImageView() {
+        addSubview(imageView)
+        imageView.frame = CGRect(x: (bounds.midX - 32.0), y: (bounds.midY - 56.0), width: 64.0, height: 64.0)
+    }
+    
+    private func showBackwardsAnimation() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            self.shapeLayer.strokeColor = self.color.withAlphaComponent(0.25).cgColor
+            let basicAnimation = CABasicAnimation(keyPath: "strokeBegin")
+            basicAnimation.toValue = 0
+            basicAnimation.duration = 5
+            basicAnimation.fillMode = .forwards
+            basicAnimation.isRemovedOnCompletion = true
+            self.shapeLayer.add(basicAnimation, forKey: "backward")
+        }
+    }
+    
+    private func showTimerEndAnimation() {
+        UIView.animate(withDuration: 1.0, delay: 0.5, options: .curveLinear, animations: {
+            self.imageView.alpha = 0.0
+            self.label.frame.origin.y -= 40.0
+        })
     }
 
 }
