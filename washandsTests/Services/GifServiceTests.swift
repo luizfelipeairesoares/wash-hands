450ab166e74feabf5424ebade0f2d0c9b2a40c0b
luizfelipeairesoares <lfelipeas@gmail.com>
2020-06-09 11:31:01 -0300

    :sparkles: Creates GifServiceTests to test requests

---
+++ washandsTests/Services/GifServiceTests.swift
@@ -0,0 +1,59 @@
+//
+//  GifServiceTests.swift
+//  washandsTests
+//
+//  Created by Luiz Felipe Aires Soares on 08/06/20.
+//  Copyright © 2020 luizfelipeairesoares. All rights reserved.
+//
+
+import XCTest
+@testable import washands
+
+class GifServiceTests: XCTestCase {
+    
+    var service: GifService!
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testRequestGifsSuccess() {
+        let mockedResponse = MockResponse()
+        service = GifService(session: BaseSuccessSessionMock(mockedData: mockedResponse.data(json: "giphyResponse")))
+        var success = false
+        let expectation = self.expectation(description: "request")
+        service.requestGifs { (result) in
+            switch result {
+            case .success:
+                success = true
+            case .failure:
+                break
+            }
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 2.0, handler: nil)
+        XCTAssert(success)
+    }
+    
+    func testRequestGifsFailure() {
+        service = GifService(session: BaseFailureSessionMock())
+        var failure = false
+        let expectation = self.expectation(description: "request")
+        service.requestGifs { (result) in
+            switch result {
+            case .success:
+                break
+            case .failure:
+                failure = true
+            }
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 2.0, handler: nil)
+        XCTAssert(failure)
+    }
+
+}
