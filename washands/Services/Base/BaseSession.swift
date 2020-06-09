f8d2bf15b3e30c72a3161585d1bf693f3051d094
luizfelipeairesoares <lfelipeas@gmail.com>
2020-06-09 20:48:32 -0300

    :art: Using APIErrors instead of plain Error

--- washands/Services/Base/BaseSession.swift
+++ washands/Services/Base/BaseSession.swift
@@ -19,7 +19,7 @@ struct BaseSession: URLSessionProtocol {
         return jsonDecoder
     }()
     
-    func request<T: Decodable>(path: String, completion: @escaping (Result<T, Error>) -> Void) {
+    func request<T: Decodable>(path: String, completion: @escaping (Result<T, APIErrors>) -> Void) {
         let baseURL: URL = URL(string: "https://washands.herokuapp.com")!
         let completeURL = baseURL.appendingPathComponent(path)
         fetch(url: completeURL, completion: completion)
@@ -27,22 +27,22 @@ struct BaseSession: URLSessionProtocol {
     
     // MARK: - Private Functions
     
-    private func fetch<T: Decodable>(url: URL, completion: @escaping (Result<T, Error>) -> Void) {
+    private func fetch<T: Decodable>(url: URL, completion: @escaping (Result<T, APIErrors>) -> Void) {
         URLSession.shared.dataTask(with: url) { (result) in
             switch result {
             case .success(let (response, data)):
                 if let statusCode = (response as? HTTPURLResponse)?.statusCode, statusCode > 299 {
-                    completion(.failure(APIErrors(code: statusCode, message: "ERROR")))
+                    completion(.failure(APIErrors.unauthorizedError))
                     return
                 }
                 do {
                     let object: T = try self.jsonDecoder.decode(T.self, from: data)
                     completion(.success(object))
                 } catch {
-                    completion(.failure(error))
+                    completion(.failure(APIErrors.parserError(message: error.localizedDescription)))
                 }
             case .failure(let error):
-                completion(.failure(error))
+                completion(.failure(APIErrors.backendError(message: error.localizedDescription)))
             }
         }.resume()
     }
