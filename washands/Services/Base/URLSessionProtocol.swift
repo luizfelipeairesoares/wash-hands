40fc98dcd70226e74ba682d275ae13b563145f6c
luizfelipeairesoares <lfelipeas@gmail.com>
2020-06-09 11:28:33 -0300

    :sparkles: Creates URLSessionProtocol

---
+++ washands/Services/Base/URLSessionProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  URLSessionProtocol.swift
+//  washands
+//
+//  Created by Luiz Felipe Aires Soares on 08/06/20.
+//  Copyright © 2020 luizfelipeairesoares. All rights reserved.
+//
+
+import Foundation
+
+protocol URLSessionProtocol {
+    
+    func request<T: Decodable>(path: String, completion: @escaping (Result<T, Error>) -> Void)
+    
+}
