012e227f5dbc0b54fedb6d200b669ec462d45e70
luizfelipeairesoares <lfelipeas@gmail.com>
2020-06-09 11:28:21 -0300

    :sparkles: Creates BaseServiceProtocol

---
+++ washands/Services/Base/BaseService.swift
@@ -0,0 +1,15 @@
+//
+//  BaseService.swift
+//  washands
+//
+//  Created by Luiz Felipe Aires Soares on 28/05/20.
+//  Copyright © 2020 luizfelipeairesoares. All rights reserved.
+//
+
+import Foundation
+
+protocol BaseServiceProtocol {
+    
+    init(session: URLSessionProtocol)
+    
+}
